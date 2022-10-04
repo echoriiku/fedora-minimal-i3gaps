d133bbfc5cc01b52700f52282ccc5ca76659bdd7
echoriiku <42903366+echoriiku@users.noreply.github.com>
2022-10-04 00:19:50 -0300

    Update fedora-setup.sh

--- fedora-setup.sh
+++ fedora-setup.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/bash
 
-# Set hostname
-sudo hostnamectl set-hostname --static "macro"
-
 # Install Rpmfusion repo
 sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 
@@ -16,17 +13,5 @@ sudo dnf install $(cat fedora.repopackages) -y
 sudo flatpak remote-add flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install flathub $(cat fedora.flatpackages) -y
 
-# enable zsh
-chsh -s $(which zsh)
-
-# setup flutter 
-mkdir -p $HOME/Dev
-cd $HOME/Dev
-wget https://storage.googleapis.com/flutter_infra/releases/stable/linux/flutter_linux_v1.0.0-stable.tar.xz
-tar xf flutter_linux_v1.0.0-stable.tar.xz
-echo export PATH=$HOME/Dev/flutter/bin:$PATH >> ~/.bashrc
-rm flutter_linux_v1.0.0-stable.tar.xz
-
-# setup Android Home
-mkdir -p $HOME/Dev/Android/Sdk
-echo export ANDROID_HOME=$HOME/Dev/Android/Sdk >> ~/.bashrc
+# enable fish
+chsh -s $(which fish)
